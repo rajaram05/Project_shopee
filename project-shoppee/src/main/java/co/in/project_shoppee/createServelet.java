d7e9917ffd833b61464682530344b9ce4f026930
Rio <dsatk@LAPTOP-MGG9306K>
2024-02-02 01:29:32 +0530

    Database established for the create page and working on login page connectivity

--- project-shoppee/src/main/java/co/in/project_shoppee/createServelet.java
+++ project-shoppee/src/main/java/co/in/project_shoppee/createServelet.java
@@ -15,43 +15,57 @@ import javax.servlet.http.HttpServletResponse;
 public class createServelet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 
 		
 		response.setContentType("text/html");
 		PrintWriter pw = response.getWriter();
-		try {			
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Ecommerce", "root", "0000");
-			String fullname = request.getParameter("fullname");
-			String username = request.getParameter("username");
-			String mail = request.getParameter("mail");
-			String phone = request.getParameter("phone");
-			String password = request.getParameter("password");
-			String conpassword = request.getParameter("conpassword");
-			String birth = request.getParameter("birth");
-			String gender = request.getParameter("gender");
-			String recovery = request.getParameter("recovery");
-			PreparedStatement ps = con.prepareStatement("insert into CustomerDetails (fullname, username, mail, phone, password, conpassword, birth, gender, recovery) values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-			ps.setString(2, fullname);
-			ps.setString(3, username);
-			ps.setString(4, mail);
-			ps.setString(5, phone);
-			ps.setString(6, password);
-			ps.setString(7, conpassword);
-			ps.setString(8, birth);
-			ps.setString(9, gender);
-			ps.setString(10, recovery);
-			ps.executeUpdate();
-			response.sendRedirect("login.html");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Ecommerce", "root", "0000");) {
+		    String fullname = request.getParameter("fullname");
+		    String username = request.getParameter("username");
+		    String mail = request.getParameter("mail");
+		    String phone = request.getParameter("phone");
+		    String password = request.getParameter("password");
+		    String conpassword = request.getParameter("conpassword");
+		    String birth = request.getParameter("birth");
+		    String gender = request.getParameter("gender");
+		    String recovery = request.getParameter("recovery");
+		    
+		    PreparedStatement ps = con.prepareStatement("insert into CustomerDetails (FullName, UserName, MailId, Phone_Number, Password, ConfirmPassword, dob, gender, recovery) values (?,?,?,?,?,?,?,?,?)");
+
+		    ps.setString(1, fullname);
+		    ps.setString(2, username);
+		    ps.setString(3, mail);
+		    ps.setString(4, phone);
+		    ps.setString(5, password);
+		    ps.setString(6, conpassword);
+		    ps.setString(7, birth);
+		    ps.setString(8, gender);
+		    ps.setString(9, recovery);
+
+
+		    int count = ps.executeUpdate();
+		    response.sendRedirect("login.html");
+		    if (count == 0) {
+		        response.sendRedirect("login.html");
+		    } else {
+		        pw.print("Try Again");
+		        response.sendRedirect("create_page.html");
+		    }
+
 		} catch (SQLException e) {
+		    e.printStackTrace();
+		    pw.write("Error in creation");
+		}
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			pw.write("error in creation");
 		}
-	
-
 	}
 }
