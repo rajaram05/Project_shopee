372141c05376d91ca859616b8e02a7909a9ea222
Rio <dsatk@LAPTOP-MGG9306K>
2024-02-02 11:44:45 +0530

    working on forgot password and home html

--- project-shoppee/src/main/java/co/in/project_shoppee/loginServelet.java
+++ project-shoppee/src/main/java/co/in/project_shoppee/loginServelet.java
@@ -28,9 +28,6 @@ public class loginServelet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	}
